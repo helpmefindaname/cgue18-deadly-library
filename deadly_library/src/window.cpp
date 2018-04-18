ce3baa517558da366b7931fba7969436fe75c992
helpmefindaname <e1526472@student.tuwien.ac.at>
2018-04-18 14:18:34 +0200

    remove ECG-lib and add refreshrate

--- deadly_library/src/window.cpp
+++ deadly_library/src/window.cpp
@@ -5,6 +5,7 @@
 Window::Window() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_REFRESH_RATE, Config::getInt("RefreshRate"));
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);  // Create an OpenGL debug context 
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
