040be8e0fd8945efce7736466cfa41e25faaa496
Iris Fiedler <e1101543@student.tuwien.ac.at>
2018-06-18 19:10:10 +0200

    add config for subdivision and normal mapping

--- deadly_library/src/Globals.cpp
+++ deadly_library/src/Globals.cpp
@@ -4,5 +4,6 @@ bool Globals::isDebug = false;
 bool Globals::showFps = false;
 bool Globals::isHelp = false;
 bool Globals::useLightMap = true;
+bool Globals::useNormalMap = true;
 int Globals::fps = 60;
 int Globals::subdivisionLevel = 1;
